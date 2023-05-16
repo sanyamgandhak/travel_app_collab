e21231ebe584a33b405dfa1e146d94c8da8fe907
Shuvam <shuvamsantra10@gmail.com>
2023-05-16 23:53:33 +0530

    Login Route, Register Route and NextAuth

--- src/app/libs/firebase.ts
+++ src/app/libs/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 import { getStorage } from "firebase/storage"
@@ -17,7 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const auth = getAuth(app)
 export const storage = getStorage(app)
 export const db = getFirestore(app)
