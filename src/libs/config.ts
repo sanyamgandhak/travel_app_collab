756fc640900ddea6ce9dafa771ce6857de00c46d
Shuvam <shuvamsantra10@gmail.com>
2023-06-07 14:58:08 +0530

    add aws url

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://roam.lazymonkey.website",
+  // baseURL: "https://roam.lazymonkey.website",
+  baseURL: "https://roam.lazymonkey.tech",
 });
