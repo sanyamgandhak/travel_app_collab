73b67486492f83ceed3f5824faf994500c90818a
zar7an <akashkishore068@gmail.com>
2023-06-12 20:18:26 +0530

    fix- changes in the days navbar

--- src/app/itinerary/page.tsx
+++ src/app/itinerary/page.tsx
@@ -2,6 +2,7 @@
 import { FC, useEffect, useState } from "react";
 import Card from "./card";
 import {FiChevronLeft, FiChevronRight} from "react-icons/fi";
+import formatDate from "./utils/format";
 
 interface Props { }
 
@@ -15,16 +16,18 @@ type dayObj = {
   day: string;
   date: string;
   month: string;
+  isActive?: boolean;
 }
 
 
 const Itinerary: FC<Props> = ({ }) => {
+  const [color, setColor] = useState<string>('#ffffff');
   const [itinerary, setItinerary] = useState([]);
   const [date, setDate] = useState<Array<dateType>>([{ day: " ", month: " ", date: " " }]);
   const [location, setLocation] = useState('');
-  const [daysArray, setDaysArray] = useState<Array<dayObj>>([{ day:'0', date:'0', month:''}, { day:'0', date:'0', month:''}, { day:'0', date:'0', month:''}, { day:'0', date:'0', month:''}, { day:'0', date:'0', month:''}]);
+  const [daysArray, setDaysArray] = useState<Array<dayObj>>([{ day:'0', date:'0', month:'', isActive: false}, { day:'0', date:'0', month:'', isActive: false}, { day:'0', date:'0', month:'', isActive: false}, { day:'0', date:'0', month:'', isActive: false}, { day:'0', date:'0', month:'', isActive: false}]);
   const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(5);
+  const [right, setRight] = useState(7);
   let shortName: { [key: string]: string } = {
     January: 'Jan',	
     February:	'Feb',
@@ -41,36 +44,16 @@ const Itinerary: FC<Props> = ({ }) => {
   }
 
 
-  const formatDate = (date: number): string => {
-    let value: string;
-    switch (date) {
-      case 1:
-        value = `${date}st`;
-        break;
-      case 2:
-        value = `${date}nd`;
-        break;
-      case 3:
-        value = `${date}rd`;
-        break;
-      default:
-        value = `${date}th`;
-    }
-    return value;
-  }
-
   const handleRightArrowClick = () => {
     if(right > date.length-1) return;
     setLeft(left+1);
     setRight(right+1);
-    // setDaysArray(date.slice(left, right));
   }
 
   const handleLeftArrowClick = () => {
     if(left < 1) return;
     setLeft(left-1);
     setRight(right-1);
-    // setDaysArray(date.slice(left, right));
   }
 
   const parseDate = (): void => {
@@ -99,20 +82,28 @@ const Itinerary: FC<Props> = ({ }) => {
 
 
   useEffect(() => {
-    if(date.length > 5) {
+    if(date.length > 7) {
       setDaysArray(date.slice(left, right));
     }
     else {
       setDaysArray(date);
     }
-    console.log(daysArray)
   }, [date, left, right])
 
+
+  const listenScrollEvent = () => {
+    if(window.scrollY > 64) {
+      setColor('#44BBA4');
+    }else {
+      setColor('#ffffff');
+    }
+  }
+
   useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent)
     const data = localStorage.getItem("ItineraryResponse");
     const locationString = localStorage.getItem("location");
     const location = locationString !== null ? JSON.parse(locationString) : null;
-
 
     setLocation(location);
     localStorage.removeItem("imageMapUrl");
@@ -127,28 +118,41 @@ const Itinerary: FC<Props> = ({ }) => {
     }
   }, []);
 
+  const handleDayClick = (index: number) => {
+    const updateDay = daysArray.map((day) => ({...day, isActive: false}));
+    updateDay[index-left] = {...daysArray[index-left], isActive: true};
+    setDaysArray(updateDay);
+  }
+
   return (
-    <div className="mt-20 mx-[88px] pb-10 scrollbar">
-      <div className="flex justify-between items-center">
-        <h1 className="w-[478px] h-[32px] text-[43px] leading-8 font-[400] font-Nunito capitalize">{location} Sightseeing</h1>
-        <div className="w-[600px] h-[76px] flex justify-center items-center p-0 m-4">
-        <FiChevronLeft className="w-[40px] h-[40px] cursor-pointer" onClick={() => handleLeftArrowClick()}/>
+    <div>
+      <div className={`flex w-[100%] h-[128px] justify-between items-center sticky top-0 left-0 bg-[${color}] px-[44px] py-[24px] shadow-[0px_8px_16px_rgba(0,0,0,.15)]`} style={{zIndex: 1}} >
+        <div className="w-[789px] h-[76px] flex justify-center items-center p-0 m-4 ">
+          <div className="w-[40px] h-[40px] flex justify-center items-center cursor-pointer">
+            <FiChevronLeft className="w-[40px] h-[25px]" onClick={() => handleLeftArrowClick()}/>
+          </div>
           {daysArray.map((dayObj, index) => {
             return (
-                <a href={`#day${left+(index+1)}`} className="w-[83px] h-[76px] flex flex-column items-center justify-center p-4 border-[2px] border-solid border-[#FFC857] rounded-3xl m-2 visited:bg-[#FFC857]  ">
-                  <div >
+                <a href={`#day${left+(index+1)}`} className={`w-[83px] h-[76px] flex flex-column items-center justify-center p-4 border-[2px] border-solid border-[#FFC857] rounded-3xl m-2 visited:bg-[#FFC857] ${daysArray[index].isActive ? "bg-[#FFC857]" : ''}`} onClick={() => handleDayClick(left+index)}
+                >
+                  <div style={{ pointerEvents: 'none' }}>
                     <h1>Day {left+(index+1)}</h1>
                     <h1 className="font-bold">{shortName[dayObj.month]} <span>{parseInt(dayObj.date)}</span></h1>
                   </div>
                 </a>
             )
           })}
-        <FiChevronRight className="w-[40px] h-[40px] cursor-pointer" onClick={() => handleRightArrowClick()}/>
+          <div className="w-[40px] h-[40px] flex justify-center items-center cursor-pointer">
+            <FiChevronRight className="w-[40px] h-[25px]"  onClick={() => handleRightArrowClick()}/>
+          </div>
         </div>
       </div>
+
+      <div className=" mx-[88px] pb-10 scrollbar">
       {itinerary.map((line, index) => {
         return <Card line={line} ParentIndex={index} dateObj={date[index % date.length]} key={index} />;
       })}
+    </div>
     </div>
   );
 };
