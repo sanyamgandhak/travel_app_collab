3b2fe5a3ce6f22df626ba8cc8bd24de94b83f147
Shuvam <shuvamsantra10@gmail.com>
2023-06-13 12:32:52 +0530

    changed url to railway

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://roam.lazymonkey.tech",
-  // baseURL: "https://roam-production.up.railway.app",
+  // baseURL: "https://roam.lazymonkey.tech",
+  baseURL: "https://roam-production.up.railway.app",
   // baseURL: "https://roam.lazymonkey.website",
 });
