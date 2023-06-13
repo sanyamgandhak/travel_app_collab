f89272dbc589c3181b33ecc4da221033fd1f553f
Shuvam <shuvamsantra10@gmail.com>
2023-06-13 13:42:58 +0530

    updated to render url

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://roam.lazymonkey.tech",
-  baseURL: "https://roam-production.up.railway.app",
-  // baseURL: "https://roam.lazymonkey.website",
+  baseURL: "https://roam-nf5b.onrender.com",
 });
