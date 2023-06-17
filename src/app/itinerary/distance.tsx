0f783ecfea069345f4627cf25ca6771134c97a37
Shuvam <shuvamsantra10@gmail.com>
2023-06-17 21:42:14 +0530

    Comments added

--- src/app/itinerary/distance.tsx
+++ src/app/itinerary/distance.tsx
@@ -1,69 +1,85 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 type props = {
   locationName: string;
-}
+};
 
 export default function Distance({ locationName }: props) {
-  const [dist, setDist] = useState<string>('0');
+  const [dist, setDist] = useState<string>("0");
   const [flag, setFlag] = useState<boolean>(true);
 
   const baseUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?`;
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchDistance = async () => {
-    
       const distanceString = localStorage.getItem("distance");
       const distance = distanceString && JSON.parse(distanceString);
 
       const storedPlaceIdObjString = localStorage.getItem("imageMapUrl"); // geting the imageMapUrl from localstorage
-      const storedPlaceIdObj = storedPlaceIdObjString && JSON.parse(storedPlaceIdObjString);
+      const storedPlaceIdObj =
+        storedPlaceIdObjString && JSON.parse(storedPlaceIdObjString);
 
       const index = distance.indexOf(locationName);
-      
-      if(index == distance.length - 1) {
+
+      if (index == distance.length - 1) {
         setFlag(false);
         return;
       }
 
       const originPlaceId = storedPlaceIdObj[distance[index]];
-      const destinationPlaceId = storedPlaceIdObj[distance[index+1]];
+      const destinationPlaceId = storedPlaceIdObj[distance[index + 1]];
 
-      const distanceResponse = await axios.get(`${baseUrl}origins=place_id:${originPlaceId}&destinations=place_id:${destinationPlaceId}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_MAP_KEY}`);
+      //Driving distance
+      const distanceResponse = await axios.get(
+        `${baseUrl}origins=place_id:${originPlaceId}&destinations=place_id:${destinationPlaceId}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_MAP_KEY}`
+      );
 
       const element = distanceResponse.data.rows[0].elements[0];
 
-      if(element.hasOwnProperty('distance') && element.distance.text !== '1 m') {
-        const kms = element.distance.text; 
+      if (
+        element.hasOwnProperty("distance") &&
+        element.distance.text !== "1 m"
+      ) {
+        const kms = element.distance.text;
         const km = kms.split(/\s+/);
         setDist(km[0]);
-        console.log(`${distance[index]} -> ${distance[index+1]}, `,distanceResponse.data);
-
-      }else {
-
-        const distanceResponse = await axios.get(`${baseUrl}origins=place_id:${originPlaceId}&destinations=place_id:${destinationPlaceId}&avoid=ferries&mode=walking&key=${process.env.NEXT_PUBLIC_GOOGLE_API_MAP_KEY}`);
+        console.log(
+          `${distance[index]} -> ${distance[index + 1]}, `,
+          distanceResponse.data
+        );
+      } else {
+        //Walking Distance
+        const distanceResponse = await axios.get(
+          `${baseUrl}origins=place_id:${originPlaceId}&destinations=place_id:${destinationPlaceId}&avoid=ferries&mode=walking&key=${process.env.NEXT_PUBLIC_GOOGLE_API_MAP_KEY}`
+        );
 
         const element = distanceResponse.data.rows[0].elements[0];
 
-        if(element.hasOwnProperty('distance')) {
-          const kms = element.distance.text; 
+        if (element.hasOwnProperty("distance")) {
+          const kms = element.distance.text;
           const km = kms.split(/\s+/);
           setDist(km[0]);
         }
-        console.log(`${distance[index]} -> ${distance[index+1]}, `,distanceResponse.data);
+        console.log(
+          `${distance[index]} -> ${distance[index + 1]}, `,
+          distanceResponse.data
+        );
       }
-    }
+    };
 
     fetchDistance();
-  }, [locationName])
+  }, [baseUrl, locationName]);
 
   return (
     <>
-      {flag 
-      ? <div className='text-[16px] font-[400] non-italic font-Roboto tracking-[0.25px]'>{(Number(dist) / 1.609).toFixed(1)} miles to next stop</div>
-      : <div>end</div>
-      }
+      {flag ? (
+        <div className="text-[16px] font-[400] non-italic font-Roboto tracking-[0.25px]">
+          {(Number(dist) / 1.609).toFixed(1)} miles to next stop
+        </div>
+      ) : (
+        <div>end</div>
+      )}
     </>
-  )
+  );
 }
